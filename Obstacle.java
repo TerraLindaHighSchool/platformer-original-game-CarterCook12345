1caeca732a138e6caa9304d04b6d131a69c3ac22
CarterCook12345 <89489627+CarterCook12345@users.noreply.github.com>
2021-10-28 15:30:49 -0700

    Game Completed

--- Obstacle.java
+++ Obstacle.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Obstacle here.
  * 
- * @author (your name) 
+ * @author Carter Cook
  * @version (a version number or a date)
  */
 public abstract class Obstacle extends Actor
